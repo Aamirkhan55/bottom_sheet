be04370241796d24dafc54c823261ba192b500c6
Amir55143 <aamirnoor55143@gmail.com>
2023-09-22 14:08:38 +0500

    Bottom/Button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -38,8 +38,26 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         child: ListView(
           padding: const EdgeInsets.all(10),
-          children: const [
-          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+          children: [
+          const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+          const SizedBox(height: 60),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                } , 
+                child: const Text('Cancle'),
+                ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                 child: const Text('Ok'),
+                 ),
+            ],
+          )
         ]),
       );
     }
